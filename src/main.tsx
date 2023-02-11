a65f3d25ca3d1c62ac2512a51888e31a5917eb3f
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-02-11 20:43:57 +0800

    Add Chakra UI
    Also removed most default styles. The app is to be styled later.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const theme = extendTheme({
+  fonts: {
+    heading: "Inter, Avenir, Helvetica, Arial, sans-serif",
+    body: "Inter, Avenir, Helvetica, Arial, sans-serif",
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
