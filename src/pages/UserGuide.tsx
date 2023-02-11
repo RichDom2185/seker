c104b56849054087e05a5e12ad664d6351d7bb16
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-02-11 22:52:28 +0800

    Style User Guide using Chakra UI
    Also made the User Guide into a true modal.

--- src/pages/UserGuide.tsx
+++ src/pages/UserGuide.tsx
@@ -1,74 +1,67 @@
-import React, { useCallback, useState } from "react";
+import {
+  Button,
+  ListItem,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  OrderedList,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
 import { Languages } from "../utils/constants";
 
-const clickableTextStyle: React.CSSProperties = {
-  cursor: "pointer",
-  textDecoration: "underline",
-};
-
 const UserGuide: React.FC = () => {
-  const [isShown, setIsShown] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const toggleIsShown = useCallback<React.MouseEventHandler>(
-    (_) => setIsShown((oldValue) => !oldValue),
-    [setIsShown]
-  );
-
-  if (!isShown) {
-    return (
-      <p
-        onClick={toggleIsShown}
-        style={{
-          ...clickableTextStyle,
-          width: "max-content",
-          marginLeft: "auto",
-          marginRight: "auto",
-        }}
-      >
-        View User Guide
-      </p>
-    );
-  }
   return (
-    <div className="modal" style={{ maxWidth: 640, margin: "0.83em auto" }}>
-      <p
-        onClick={toggleIsShown}
-        style={{ ...clickableTextStyle, textAlign: "right", marginTop: 0 }}
-      >
-        Close <i>×</i>
-      </p>
-      <ol
-        style={{
-          columnCount: 2,
-          columnGap: "3.6em",
-          textAlign: "left",
-        }}
-      >
-        <li>
-          Connect your SPIKE Prime to your computer via USB.
-          <br />
-          <em>
-            <strong>Note:</strong> You can also connect via Bluetooth but
-            reliability will not be guaranteed.
-          </em>
-        </li>
-        <li>
-          Select the appropriate language mode: {Languages.SOURCE_THREE} or{" "}
-          {Languages.PYTHON}
-        </li>
-        <li>After finishing your code, click the "Run on Device" button.</li>
-        <li>
-          Select the correct port from the pop-up that is shown.
-          <br />
-          <em>
-            <strong>Note:</strong> SEKER only supports Chromium-based browsers
-            version 89 and onwards. If you do not see a pop-up, this means your
-            browser is not supported.
-          </em>
-        </li>
-        <li>The SPIKE Prime should start running your code.</li>
-      </ol>
-    </div>
+    <>
+      <Button variant="link" onClick={onOpen} fontWeight="normal">
+        View User Guide
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>User Guide</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody paddingBottom={0}>
+            <OrderedList>
+              <ListItem>
+                Connect your SPIKE Prime to your computer via USB.
+                <br />
+                <em>
+                  <strong>Note:</strong> You can also connect via Bluetooth but
+                  reliability will not be guaranteed.
+                </em>
+              </ListItem>
+              <ListItem>
+                Select the appropriate language mode: {Languages.SOURCE_THREE}{" "}
+                or {Languages.PYTHON}
+              </ListItem>
+              <ListItem>
+                After finishing your code, click the "Run on Device" button.
+              </ListItem>
+              <ListItem>
+                Select the correct port from the pop-up that is shown.
+                <br />
+                <em>
+                  <strong>Note:</strong> SEKER only supports Chromium-based
+                  browsers version 89 and onwards. If you do not see a pop-up,
+                  this means your browser is not supported.
+                </em>
+              </ListItem>
+              <ListItem>
+                The SPIKE Prime should start running your code.
+              </ListItem>
+            </OrderedList>
+          </ModalBody>
+          <ModalFooter paddingTop={0} />
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
