b9cf8dc1e77ff3748cb9393b9355f608bd6d5e35
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-02-16 03:03:58 +0800

    Add sample program to tour the API

---
+++ src/programs/source3/apiTour.js
@@ -0,0 +1,4 @@
+// SEKER API Tour
+
+// Shows the below text on the 5x5 display, one character at a time
+spike_showText("Hello from SPIKE Prime!");
--- src/programs/source3/index.ts
+++ src/programs/source3/index.ts
@@ -1,9 +1,11 @@
+import sampleProgramApiTour from "./apiTour.js?raw";
 import sampleProgramMotor from "./motor.js?raw";
 import sampleProgramMusic from "./music.js?raw";
 import sampleProgramSensor from "./sensor.js?raw";
 import sampleProgramZigzag from "./zigzag.js?raw";
 
 export const sampleSourceThreePrograms: ReadonlyArray<string> = [
+  sampleProgramApiTour,
   sampleProgramMotor,
   sampleProgramMusic,
   sampleProgramSensor,
