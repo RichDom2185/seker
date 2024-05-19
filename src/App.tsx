2e25384e43ddfb78dfaa9204f3d17c260d3d8006
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-20 03:42:28 +0800

    Refactor `App.tsx`
    * Move type declarations to a separate file
    * Reformat type declarations
    * Use anonymous function for App component
    * Use `React.FC` typing for App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,8 @@ import EditorPage from "./pages/EditorPage";
 
 import "./App.css";
 
-/*
- * References:
- * https://github.com/microsoft/pxt-ev3/blob/bef4ebac43414beb900f04812f7c5101a192c22e/editor/deploy.ts
- * https://wicg.github.io/serial/
- */
-declare type SerialOptions = any;
-declare type SerialPortInfo = any;
-declare class SerialPort {
-  open(options?: SerialOptions): Promise<void>;
-  close(): void;
-  readonly readable: any;
-  readonly writable: any;
-  getInfo(): SerialPortInfo;
-}
-declare interface Serial extends EventTarget {
-  onconnect: React.EventHandler<any>;
-  ondisconnect: React.EventHandler<any>;
-  getPorts(): Promise<SerialPort[]>;
-  requestPort(options?: any): Promise<SerialPort>;
-}
-
-declare global {
-  interface Navigator {
-    readonly serial: Serial;
-  }
-}
-
-function App() {
+const App: React.FC = () => {
   return <EditorPage />;
-}
+};
 
 export default App;
