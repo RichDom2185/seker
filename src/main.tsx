eb38cc2915780b35257e0cdfc42670fd9119f60c
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-20 03:33:28 +0800

    Set up auto dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeScript,
+  ThemeConfig,
+  extendTheme,
+} from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const config: ThemeConfig = {
+  initialColorMode: "system",
+  useSystemColorMode: true,
+};
+
 const theme = extendTheme({
+  config,
   fonts: {
     heading: "Inter, Avenir, Helvetica, Arial, sans-serif",
     body: "Inter, Avenir, Helvetica, Arial, sans-serif",
@@ -13,6 +24,7 @@ const theme = extendTheme({
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
