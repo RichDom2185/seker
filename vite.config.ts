fca8c7e88241bd3fa177ddee5613fca2cbf46195
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2026-03-24 15:23:41 +0800

    Upgrade Vite ecosystem to v8

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  legacy: {
+    // react-ace needs this to work with Vite 8
+    inconsistentCjsInterop: true,
+  },
   resolve: {
     alias: [
       // Use UMD bundle for xmlhttprequest-ts dependency
